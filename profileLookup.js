cd5baaceb0b8311cb4c8a8506e629813eff038e8
PhilippNikitin <54664800+PhilippNikitin@users.noreply.github.com>
2019-09-24 10:50:46 +0300

    Add files via upload

---
+++ profileLookup.js
@@ -0,0 +1,54 @@
+//Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(name, prop){
+
+// Only change code below this line
+
+for (var i = 0; i < contacts.length; i++ ) {
+if (name == contacts[i].firstName && contacts[i].hasOwnProperty(prop))
+{return contacts[i][prop];}
+};
+for (var i = 0; i < contacts.length; i++ ) {
+  if (name == contacts[i].firstName) {
+      if (contacts[i].hasOwnProperty(prop) == false) 
+      {return "No such property";}
+};
+for (var i = 0; i < contacts.length; i++ ) {
+  if (name != contacts[i].firstName)  
+  {return "No such contact";}
+};}}
+
+// Only change code above this line
+
+
+
+
+// Change these values to test your function
+lookUpProfile("Akira", "likes");
